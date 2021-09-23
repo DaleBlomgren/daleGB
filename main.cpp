2d604ec9a5704514064e4dbdb0b5dda687e04fe5
Dale Blomgren <DaleBlom@gmail.com>
2021-09-23 14:14:43 -0600

    added dlls

--- main.cpp
+++ main.cpp
@@ -5,8 +5,25 @@
 //---------------------------------------------------------------------------//
 // Written by Dale Blomgren													 //
 //---------------------------------------------------------------------------//
+//																			 //
+///////////////////////////////////////////////////////////////////////////////
+
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+
 
 int main(int argc, char* argv[]) {
 
+	if (!glfwInit()) {
+		std::string noted;
+		printf("glfw initiation failure\n Press Enter to Exit: \n");
+		std::cin >> noted;
+		glfwTerminate();
+		exit(1);
+	}
+
+	glfwTerminate();
 	return 0;
 }
