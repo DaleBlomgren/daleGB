87a2e0b70d2b252c38fb256b54c6f85ba87aeb3c
Dale Blomgren <DaleBlom@gmail.com>
2021-09-24 10:56:39 -0600

    GLFW init stuff

--- main.cpp
+++ main.cpp
@@ -13,26 +13,49 @@
 #include <string>
 #include <iostream>
 
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+
+
+static void error_callback(int error, const char* description) {
+	fprintf(stderr, "Error: %s\n", description); 
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main(int argc, char* argv[]) {
 
 	GLFWwindow* window;
+
+	glfwSetErrorCallback(error_callback);
 
 	if (!glfwInit()) {
 		std::string noted;
 		printf("glfw initiation failure\n Press Enter to Exit: \n");
 		std::cin >> noted;
 
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	//GB resolution is 160(w) x 144(h)
 	window = glfwCreateWindow(160, 144, "daleGB pre-Alpha", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
-		exit(2);
+		exit(EXIT_FAILURE);
 	}
 
+	glfwMakeContextCurrent(window);
+
+
+	//Primary loop
+	while (!glfwWindowShouldClose(window)) {
+
+	}
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
