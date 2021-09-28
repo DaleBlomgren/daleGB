b609048023ba88e93eb6383bab913ddf0b4e5580
Dale Blomgren <DaleBlom@gmail.com>
2021-09-28 12:43:53 -0600

    started GB implementation

---
+++ src/CPU.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////////
+//This is an implementation of a Sharp LR35902 Processor				 //
+/////////////////////////////////////////////////////////////////////////// 
+// Specs:																 //
+// CPU freq  : 4.194304 MHz												 //
+// Work Ram  : 8 KB														 //
+// Video Ram : 8 KB														 //
+// Resolution: 8 KB														 //
+// OBJ ("Sprites"): 8 x 8 or 8 x 16; max 40 per screen, 10 per line		 //
+// Palettes: BG: 1x4, OBJ: 2 x 3										 //
+// Colors: 4 shades of green											 //
+// Horizontal sync: 9.198 KHz											 //
+// Vertical sync:   59.73 Hz											 //
+//																		 //
+///////////////////////////////////////////////////////////////////////////
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////
+// DaleGB																	 //
+//---------------------------------------------------------------------------//
+// A Gameboy Emulator implemented in C++ with the help of GLFW				 //	
+//---------------------------------------------------------------------------//
+// Written by Dale Blomgren													 //
+//---------------------------------------------------------------------------//
+//																			 //
+///////////////////////////////////////////////////////////////////////////////
+
+//#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+
+
+static void error_callback(int error, const char* description) {
+	fprintf(stderr, "Error: %s\n", description); 
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+
+int main(int argc, char* argv[]) {
+
+	GLFWwindow* window;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit()) {
+		std::string noted;
+		printf("glfw initiation failure\n Press Enter to Exit: \n");
+		std::cin >> noted;
+
+		exit(EXIT_FAILURE);
+	}
+	//GB resolution is 160(w) x 144(h)
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "daleGB pre-Alpha", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+	//Primary loop
+	while (!glfwWindowShouldClose(window)) {
+		
+		
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
