c76dbc583ee50e42b334f3a8dcd544039bf08bc8
Dale Blomgren <DaleBlom@gmail.com>
2021-11-29 00:41:20 -0800

    More Blueprint

---
+++ src/CPU.h
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////////////////////
+//This is an implementation of a Sharp LR35902 Processor				 //
+/////////////////////////////////////////////////////////////////////////// 
+// Specs:																 //
+// CPU freq  : 4.194304 MHz												 //
+// Work Ram  : 8 KB														 //
+// Video Ram : 8 KB														 //
+// Resolution: 160 x 144 (20 x 18 tiles)								 //
+// OBJ ("Sprites"): 8 x 8 or 8 x 16; max 40 per screen, 10 per line		 //
+// Palettes: BG: 1x4, OBJ: 2 x 3										 //
+// Colors: 4 shades of green											 //
+// Horizontal sync: 9.198 KHz											 //
+// Vertical sync:   59.73 Hz											 //
+// Machine cycle: 1.05MHz												 //
+// Clock cycle: 4.19 MHz  (4 Machine cycles in a Clock cycle)			 //
+// Memory register goes up to FFFF										 //
+///////////////////////////////////////////////////////////////////////////
+
+#include <stdint.h>
+
+class CPU {
+	public:
+		CPU();
+		~CPU();
+
+		
+		void emulateCycle();
+
+
+	private:
+
+		unsigned short pc;
+		unsigned short opcode;
+		unsigned short sp;		//stack pointer
+		unsigned short I;		//index register
+
+		unsigned char V[8];	    //V-registers 
+
+		uint8_t memory[65535]; 
+		
+		bool initialRead(const char* filename);
+		int init();
+};
